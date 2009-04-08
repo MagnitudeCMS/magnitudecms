a0213bb9b5949ae8fc53ba17f22a3f057d173dee
Nicholas Orr <nicholas.orr@zxgen.net>
2009-04-08 20:01:21 +1000

    updated gems

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.9"
+merb_gems_version = "1.0.10"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-core", merb_gems_version
@@ -18,22 +18,22 @@ dependency "merb-auth-more", merb_gems_version
 dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
-
 dependency "merb-gen", merb_gems_version
 
-dependency "mongrel"
+# webserver
+dependency "thin"
+
+# datastore
+dependency "jchris-couchrest", "0.23.0", :require_as => "couchrest"   # $ gem sources -a http://gems.github.com
+
+# view
 dependency "merb_viewfu"
-
 dependency "maruku"
-# $ gem sources -a http://gems.nicholasorr.com
-dependency "haml", "~>2.1.0"
+dependency "haml", "2.1.0"                                            # $ gem sources -a http://gems.nicholasorr.com
 dependency "merb-haml", merb_gems_version
-dependency "chriseppstein-compass", "0.5.0", :require_as => "compass"
-
-dependency "jchris-couchrest", "0.17.0", :require_as => "couchrest"
+dependency "chriseppstein-compass", "0.6.1", :require_as => "compass" # $ gem sources -a http://gems.github.com
 
 # testing
 dependency "cucumber"
-# $ gem sources -a http://gems.github.com
-dependency "auxesis-webrat", "0.4.1", :require_as => "webrat"
-dependency "david-merb_cucumber", :require_as => "merb_cucumber"
+dependency "auxesis-webrat", "0.4.1", :require_as => "webrat"         # $ gem sources -a http://gems.github.com
+dependency "david-merb_cucumber", :require_as => "merb_cucumber"      # $ gem sources -a http://gems.github.com
