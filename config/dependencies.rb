7098321737b44221fa60e3ce1905bf1f3b6d6ebd
Nicholas Orr <nicholas.orr@zxgen.net>
2009-04-08 20:06:24 +1000

    final touches to pretty file :)

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,7 +1,5 @@
-# dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
 merb_gems_version = "1.0.10"
 
-# For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-core", merb_gems_version
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version
