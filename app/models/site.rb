29853a0d4f46c4189b659404b73776bbd31bcfb9
Nicholas Orr <nicholas.orr@zxgen.net>
2009-04-13 04:50:15 +1000

    Need a way to check a user is an actual admin of a site. added view by_admin

--- app/models/site.rb
+++ app/models/site.rb
@@ -14,12 +14,26 @@ class Site < BaseModel
   # having it separate from domain[0] enables any domain to be default
   property :default_domain
   property :couchdb
-
+  # need a way to check if logged in person is an actual admin of this particular site
+  property :admins, :default => []
+  
   view_by :domain, :map => <<MAP
 function(doc) {
   if(doc["couchrest-type"] == "Site" && doc.domains) {
-    doc.domains.forEach(function(domain){
+    doc.domains.forEach(function(domain){    
       emit(domain, null);
+    });
+  }
+}
+MAP
+
+  view_by :admin, :map => <<MAP
+function(doc) {
+  if(doc["couchrest-type"] == "Site" && doc.domains && doc.admins) {
+    doc.domains.forEach(function(domain){
+      doc.admins.forEach(function(admin){
+        emit(admin + "|" + domain, null);
+      });
     });
   }
 }
@@ -32,11 +46,18 @@ MAP
   validates_present :default_domain
   validates_present :name
   validates_present :couchdb
+  validates_present :admins
 
   def self.get_couchdb(domain)
     s = self.by_domain :key => domain
     return s[0].couchdb unless s.empty?
     return nil
+  end
+  
+  def self.is_user_admin?(user,domain)
+    s = self.by_admin :key => "#{user}|#{domain}", :reduce => true
+    return true unless s["rows"].empty?
+    return false
   end
 
 end
