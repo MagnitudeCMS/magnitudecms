43209a18358eed4cc2c48f636078127a4e581d0d
Nicholas Orr <nicholas.orr@zxgen.net>
2009-10-06 09:51:07 +1100

    need to reload templates in prod as they change in the db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
+  c[:reload_templates] = true
   c[:log_level] = :error
   
   c[:log_file]  = Merb.root / "log" / "production.log"
